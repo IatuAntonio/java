1540389a13e3d78fa9cd79b422354dc3ded5912f
IatuAntonio <79212418+IatuAntonio@users.noreply.github.com>
2025-10-17 03:59:01 +0300

    compulsory

---
+++ Lab2/Compulsory/src/main/java/com/example/Compulsory/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.Compulsory;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${greeting.message}")
+    private String greetingMessage;
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return greetingMessage;
+    }
+
+}
