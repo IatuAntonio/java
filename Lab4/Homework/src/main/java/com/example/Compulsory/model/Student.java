b388bb2102070a8863cef3abddda509df76001f0
IatuAntonio <79212418+IatuAntonio@users.noreply.github.com>
2025-11-01 13:53:23 +0200

    done compulsory

---
+++ Lab4/Homework/src/main/java/com/example/Compulsory/model/Student.java
@@ -0,0 +1,58 @@
+package com.example.Compulsory.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+    private String name;
+    private String email;
+    private int year;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
